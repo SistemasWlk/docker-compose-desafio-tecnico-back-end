017fdac7ebc4bbd3fbf52816b531e30e932215f2
SistemasWlk <sistemaswlk@gmail.com>
2023-03-05 16:44:43 -0300

    wrench: chore: Configurando seed patient
    - Criando a seed e factory de patient

--- laravel-docker-desafio-tecnico-back-end/database/factories/PatientFactory.php
+++ laravel-docker-desafio-tecnico-back-end/database/factories/PatientFactory.php
@@ -2,13 +2,22 @@
 
 namespace Database\Factories;
 
+use App\Models\Partient;
 use Illuminate\Database\Eloquent\Factories\Factory;
+use Illuminate\Support\Str;
 
 /**
  * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Partient>
  */
 class PatientFactory extends Factory
 {
+    /**
+     * The name of the factory's corresponding model.
+     *
+     * @var string
+     */
+    protected $model = Partient::class;
+
     /**
      * Define the model's default state.
      *
@@ -17,7 +26,21 @@ class PatientFactory extends Factory
     public function definition()
     {
         return [
-            //
+            'name' => $faker->name,
+            'image' => $faker->image,
+            'cpf' => $faker->unique()->cpf,
+            'cns' => $faker->unique()->cns,
+            'birth_date' => $faker->birth_date,
+            'mother' => $faker->mother,
+            'father' => $faker->father,
+            'cep' => $faker->cep,
+            'address' => $faker->address,
+            'number' => $faker->number,
+            'complement' => $faker->complement,
+            'neighborhood' => $faker->neighborhood,
+            'city' => $faker->city,
+            'state' => $faker->state,
         ];
     }
 }
+
--- laravel-docker-desafio-tecnico-back-end/database/seeders/PatientsTableSeeder.php
+++ laravel-docker-desafio-tecnico-back-end/database/seeders/PatientsTableSeeder.php
@@ -4,9 +4,12 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use App\Models\Patient;
+
 
 class PatientsTableSeeder extends Seeder
 {
+
     /**
      * Run the database seeds.
      *
@@ -14,6 +17,85 @@ class PatientsTableSeeder extends Seeder
      */
     public function run()
     {
-        //
+        Patient::create([
+            'name' => 'patient_01',
+            'image' => 'patients/patient_01',
+            'cpf' => '23282514042',
+            'cns' => '238190590200002',
+            'birth_date' => '1988-09-09',
+            'mother' => 'mother_01',
+            'father' => 'father_01',
+            'cep' => '76550000',
+            'address' => 'Rua 01',
+            'number' => '01',
+            'complement' => 'QD 01 LT 01',
+            'neighborhood' => 'Centro',
+            'city' => 'Porangatu',
+            'state' => 'Goias'
+        ]);
+        Patient::create([
+            'name' => 'patient_01',
+            'image' => 'patients/patient_02',
+            'cpf' => '88254712034',
+            'cns' => '115381190860009',
+            'birth_date' => '1988-09-09',
+            'mother' => 'mother_02',
+            'father' => 'father_02',
+            'cep' => '76550000',
+            'address' => 'Rua 02',
+            'number' => '02',
+            'complement' => 'QD 01 LT 02',
+            'neighborhood' => 'Centro',
+            'city' => 'Porangatu',
+            'state' => 'Goias',
+        ]);
+        Patient::create([
+            'name' => 'patient_03',
+            'image' => 'patients/patient_03',
+            'cpf' => '71595925066',
+            'cns' => '171873824470002',
+            'birth_date' => '1988-09-09',
+            'mother' => 'mother_03',
+            'father' => 'father_03',
+            'cep' => '76550000',
+            'address' => 'Rua 03',
+            'number' => '03',
+            'complement' => 'QD 01 LT 03',
+            'neighborhood' => 'Centro',
+            'city' => 'Porangatu',
+            'state' => 'Goias',
+        ]);
+        Patient::create([
+            'name' => 'patient_04',
+            'image' => 'patients/patient_04',
+            'cpf' => '01226343066',
+            'cns' => '241767785320008',
+            'birth_date' => '1988-09-09',
+            'mother' => 'mother_04',
+            'father' => 'father_04',
+            'cep' => '76550000',
+            'address' => 'Rua 04',
+            'number' => '04',
+            'complement' => 'QD 01 LT 04',
+            'neighborhood' => 'Centro',
+            'city' => 'Porangatu',
+            'state' => 'Goias',
+        ]);
+        Patient::create([
+            'name' => 'patient_05',
+            'image' => 'patients/patient_05',
+            'cpf' => '97320676087',
+            'cns' => '987492205470018',
+            'birth_date' => '1988-09-09',
+            'mother' => 'mother_05',
+            'father' => 'father_05',
+            'cep' => '76550000',
+            'address' => 'Rua 05',
+            'number' => '05',
+            'complement' => 'QD 01 LT 05',
+            'neighborhood' => 'Centro',
+            'city' => 'Porangatu',
+            'state' => 'Goias',
+        ]);
     }
 }
